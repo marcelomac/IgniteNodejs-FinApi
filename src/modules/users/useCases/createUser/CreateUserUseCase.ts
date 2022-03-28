a002f26ab666f3572c845e65b2d701a009365ab6
Marcelo A. Macagnan <marcelo.a.mac@gmail.com>
2022-03-28 17:40:19 -0300

    20222803

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -3,6 +3,7 @@ import { hash } from 'bcryptjs';
 
 import { CreateUserError } from "./CreateUserError";
 
+
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "./ICreateUserDTO";
 
